395bcfe582bc26a673092fb1ad5b771650a518bd
xinetzone <xinzone@outlook.com>
2021-01-12 17:04:47 +0800

    modified:   utils/markdown.js

--- utils/markdown.js
+++ utils/markdown.js
@@ -2,14 +2,12 @@
 
 /* # 将 `code` 转换 <inline-code>code</inline-code> */
 function inlineCodeTomd() {
-    let paras = document.querySelectorAll('p')
-    for (let block of paras) {
-        // 添加 内联代码样式
-        const text = block.innerHTML.replace(/\`(\w+)\`/g, '<inline-code>$1</inline-code>')
-        // 添加 HTML 元素样式
-        const text1 = text.replace(/\`html{(\w+)}\`/g, '<inline-code>&lt;$1></inline-code>')
-        block.innerHTML = text1
-    }
+    let body = document.querySelector('body')
+    // 添加 内联代码样式
+    const text = body.innerHTML.replace(/\`(\w+)\`/g, '<inline-code>$1</inline-code>')
+    // 添加 HTML 元素样式
+    const text1 = text.replace(/\`html{(\w+)}\`/g, '<inline-code>&lt;$1></inline-code>')
+    body.innerHTML = text1
 }
 
 export { inlineCodeTomd}
