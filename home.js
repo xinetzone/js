fcf161b913a98420604a671d3e40c7c4f7e50218
xinetzone <xinzone@outlook.com>
2021-01-12 09:14:02 +0800

    添加对 <p> 的 \n 的处理

--- home.js
+++ home.js
@@ -1,12 +1,13 @@
 'use strict'
 
 import { addHead } from './head.js'
-import { deleteWhiteSpace } from './utils/delete-white-space.js'
+import { deletePreWhiteSpace, deleteParaWhiteSpace } from './utils/delete-white-space.js'
 
 // 主函数
 function main() {
   addHead() // 添加 <head> 元素信息
-  deleteWhiteSpace()
+  deletePreWhiteSpace()
+  deleteParaWhiteSpace()
 }
 
 /* 函数调用 */
--- utils/delete-white-space.js
+++ utils/delete-white-space.js
@@ -1,7 +1,7 @@
 'use strict'
 
-/* 删除空白符 */
-function deleteWhiteSpace() {
+/* # 删除空白符 */
+function deletePreWhiteSpace() {
     let codeBlocks = document.querySelectorAll('pre > code')
 
     for (let block of codeBlocks) {
@@ -18,4 +18,21 @@ function deleteWhiteSpace() {
     }
 }
 
-export {deleteWhiteSpace}
+function deleteParaWhiteSpace() {
+    let para = document.querySelectorAll('p')
+
+    for (let block of para) {
+        // 删除代码块首尾空白符
+        const text = block.textContent.trim()
+        // 删除每行的开头的多余空白符
+        let array = text.split('\n')
+        const lastElement = array[array.length - 1]
+        const spaceNum = lastElement.search(/\S/)
+        const spaceReg = RegExp(`(\\s{${spaceNum}})`)
+        const array1 = array.map(strings => strings.replace(spaceReg, ''))
+        let outText = array1.join('')
+        block.textContent = outText
+    }
+}
+
+export { deletePreWhiteSpace, deleteParaWhiteSpace }
