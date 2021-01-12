7e0034a4bb024c20adc2245fb0595efba4394924
xinetzone <xinzone@outlook.com>
2021-01-12 10:18:01 +0800

    处理 <p> 中的换行符

--- utils/delete-white-space.js
+++ utils/delete-white-space.js
@@ -7,7 +7,7 @@ function deletePreWhiteSpace() {
     for (let block of codeBlocks) {
         // 删除代码块首尾空白符
         const text = block.textContent.trim()
-        // 删除每行的开头的多余空白符
+        /* # 删除每行的开头的多余空白符 */
         let array = text.split('\n')
         const lastElement = array[array.length - 1]
         const spaceNum = lastElement.search(/\S/)
@@ -19,19 +19,14 @@ function deletePreWhiteSpace() {
 }
 
 function deleteParaWhiteSpace() {
-    let para = document.querySelectorAll('p')
-
-    for (let block of para) {
+    let paras = document.querySelectorAll('p')
+    for (let para of paras) {
         // 删除代码块首尾空白符
-        const text = block.textContent.trim()
-        // 删除每行的开头的多余空白符
-        let array = text.split('\n')
-        const lastElement = array[array.length - 1]
-        const spaceNum = lastElement.search(/\S/)
-        const spaceReg = RegExp(`(\\s{${spaceNum}})`)
-        const array1 = array.map(strings => strings.replace(spaceReg, ''))
-        let outText = array1.join('')
-        block.textContent = outText
+        const text = para.innerHTML.trim()
+        /* # 删除多余的换行符以及换行符后面的空白符 */
+        const spaceReg = /[\n\r\s+]/
+        let outText = text.replace(spaceReg, '')
+        para.innerHTML = outText
     }
 }
 
