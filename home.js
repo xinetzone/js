b027969ecd154028dbe9d32f659c1af5a232196f
xinetzone <xinzone@outlook.com>
2021-01-13 12:29:31 +0800

    添加 body 主题

--- home.js
+++ home.js
@@ -10,6 +10,9 @@ function main() {
   deletePreWhiteSpace()
   deleteParaWhiteSpace()
   inlineCodeTomd()
+  let body = document.body
+  // 添加主题
+  body.classList.add('w3-padding', 'w3-pale-yellow')
 }
 
 /* 函数调用 */
