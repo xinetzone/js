c34a281cc784f0431e003e94228ee9e5ea2b9774
xinetzone <xinzone@outlook.com>
2021-03-17 14:39:32 +0800

    Update

--- interface.js
+++
@@ -1,7 +0,0 @@
-function openInterface(interfaceName) {
-  let x = document.getElementsByClassName("interface");
-  for (let i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  document.getElementById(interfaceName).style.display = "block";
-}
